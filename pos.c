d7f22e6680e6dfc13c761697f43ed45a7ba729fc
snehasss <30317619+snehasss@users.noreply.github.com>
2017-07-20 18:00:09 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter ant number:");
+scanf("%d",&num);
+if(num>0)
+{
+printf(" Number is POSITIVE ");
+}
+else if(num<0)
+{
+printf(" Number is NEGATIVE ");
+}
+else
+{
+printf(" Number is ZERO ");
+}
+return 0;
+}
