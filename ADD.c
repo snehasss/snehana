f2d5b093c2375ca3e798458873f0937bdbe17720
snehasss <30317619+snehasss@users.noreply.github.com>
2017-08-04 16:43:57 +0530

    Create ADD.c

---
+++ ADD.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() 
+{
+int *ptr1, *ptr2,num;
+printf("\nEnter two numbers : ");
+scanf("%d %d", ptr1, ptr2);
+num = *ptr1 + *ptr2;
+printf("Sum = %d", num);
+return (0);
+}
